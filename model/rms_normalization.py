cf0a6c32f78c6d57c3fbd9b2e56c6d1cceee638c
Alisson Silva <42107837+alissonsz1@users.noreply.github.com>
2026-07-22 13:54:58 -0300

    Implement RMS normalization (PyTorch 4/4)

---
+++ model/rms_normalization.py
@@ -0,0 +1,22 @@
+import numpy as np
+from typing import List
+
+
+class Solution:
+    def rms_norm(self, x: List[float], gamma: List[float], eps: float) -> List[float]:
+        x = np.array(x);
+        gamma = np.array(gamma)
+        
+
+        rms = np.sqrt(eps + np.mean(x**2));
+
+        y = x/rms * gamma;
+
+        return np.round(y,4)
+
+
+        
+        # Implement RMS Normalization (similar to LayerNorm but without mean centering or beta)
+        # Normalize x, then scale by gamma
+        # Return result rounded to 4 decimal places as a list
+        pass
